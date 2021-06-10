8f8552d6c486b25e89fdd46ce657e2a141353b8f
Patrick Sattelberger <sattelbergerp@gmail.com>
2021-06-10 01:28:23 -0400

    Notification types are now also created on app startup

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,7 +1,13 @@
 from django.apps import AppConfig
+from django.db import connection
+from .utils import create_notification_types
 
 class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
-   
+    def ready(self):
+        from .models import NotificationType
+        if NotificationType._meta.db_table in connection.introspection.table_names():
+            # Create notification types only if the table exists in the database
+            create_notification_types()
